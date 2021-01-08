d701562d0c9386e56873327dbc686c7e1bcf16e8
bookgvi <bookgvi@gmail.com>
2021-01-08 12:40:48 +0300

    BooksMock model

---
+++ src/main/java/Model/BooksListMock.java
@@ -0,0 +1,47 @@
+package Model;
+
+import javax.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestScoped
+public class BooksListMock {
+  private List<Book> booksList = new ArrayList<Book>();
+
+  public BooksListMock() {
+    Book book = new Book(1, "QQQ1");
+    booksList.add(book);
+    book = new Book(2, "QQQ2");
+    booksList.add(book);
+  }
+
+  public List<Book> getBooksList() {
+    return booksList;
+  }
+
+  private static class Book {
+    private int isbn;
+    private String title;
+
+    Book(int isbn, String title) {
+      this.isbn = isbn;
+      this.title = title;
+    }
+
+    public void setIsbn(int isbn) {
+      this.isbn = isbn;
+    }
+
+    public int getIsbn() {
+      return isbn;
+    }
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    public String getTitle() {
+      return title;
+    }
+  }
+}
